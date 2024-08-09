6f8739005285a7189cc359dcc6fe789819b0e4a4
Wcoder547 <mw5667155@.com>
2024-08-09 09:18:04 +0500

    add hello world commit

--- 12MegaProject/src/components/AuthLayout.jsx
+++ 12MegaProject/src/components/AuthLayout.jsx
@@ -1,30 +1,21 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom"; 
+import { useNavigate } from "react-router-dom";
 
+export default function Protected({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const [loader, setloader] = useState(true);
+  const authStatus = useSelector((state) => state.auth.status);
 
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
 
+    setloader(false);
+  }, [authStatus, navigate, authentication]);
 
-export default  function Protected({children,authentication= true}) {
-    const navigate = useNavigate()
-    const [loader,setloader] = useState(true)
-    const authStatus = useSelector((state)=>state.auth.status)
-
-    useEffect(() => {
-      if(authentication && authStatus!==authentication){
-        navigate("/login")
-      }
-      
-      else if(!authentication && authStatus!==authentication){
-        navigate("/")
-      }
-      
-      setloader(false)
-    }, [authStatus,navigate,authentication])
-    
-  return loader ? <div>Loading .. </div> : <> {children} </>
-
+  return loader ? <div>Loading .. </div> : <> {children} </>;
 }
-
-
-
