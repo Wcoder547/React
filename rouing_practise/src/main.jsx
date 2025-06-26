1653bd6913d44adf605ded84e4c270dd75d5d480
wcoder547 <mw5667155@gmail.com>
2025-06-26 18:39:52 +0500

    Refactor components for improved code consistency and readability

--- rouing_practise/src/main.jsx
+++ rouing_practise/src/main.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom"
-import Layout from './Layout'
-import Home from './components/Home/Home'
-import About from "./components/AboutUS/AboutUs"
-import Contact from "./components/ContactUs/ContactUs"
-import User from './components/User'
-import Github from './components/Github/Github'
-import { GithubInfoLoader } from './components/Github/Github'
-
-
-
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./components/Home/Home";
+import About from "./components/AboutUS/AboutUs";
+import Contact from "./components/ContactUs/ContactUs";
+import User from "./components/User";
+import Github from "./components/Github/Github";
+import { GithubInfoLoader } from "./components/Github/Github";
 
 // const router = createBrowserRouter([{
 //   path:'/',
@@ -20,7 +22,7 @@ import { GithubInfoLoader } from './components/Github/Github'
 //   children:[{
 //     path:"",
 //     element:<Home/>
-//   },{   
+//   },{
 //     path:"about",
 //     element:<About/>
 //   },
@@ -32,19 +34,19 @@ import { GithubInfoLoader } from './components/Github/Github'
 // }])
 
 const router = createBrowserRouter(
-createRoutesFromElements(
-  <Route path='/' element={<Layout/>}>
-    <Route path='' element = {<Home/>}/>
-    <Route path='About' element= {<About/>}/>
-    <Route path='Contact' element = {<Contact/>}/>
-    <Route  path='User/:userId' element = {<User/>}/>
-    <Route loader={GithubInfoLoader}path='Github' element= {<Github/>}/>
-  </Route>
-)
-)
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="About" element={<About />} />
+      <Route path="Contact" element={<Contact />} />
+      <Route path="User/:userId" element={<User />} />
+      <Route loader={GithubInfoLoader} path="Github" element={<Github />} />
+    </Route>
+  )
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-< RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
